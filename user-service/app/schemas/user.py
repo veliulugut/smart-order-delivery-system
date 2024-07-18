3371e024f57b37d795f40f1a6b4c27fbcf97c676
Veli Ulugut <veliulugut1@gmail.com>
2024-07-18 10:37:31 +0300

    user dto model

---
+++ user-service/app/schemas/user.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, EmailStr
+
+
+class RegisterUserRequest(BaseModel):
+    name: str
+    email: EmailStr
+    password: str
+    
+    
+class VerifyUserRequest(BaseModel):
+    token: str
+    email: EmailStr
+    
+class EmailRequest(BaseModel):
+    email: EmailStr
+    
+class ResetRequest(BaseModel):
+    token: str
+    email: EmailStr
+    password: str
+    
